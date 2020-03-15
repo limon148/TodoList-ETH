ff05f26d2beae36e3552105271983fb8ffe36650
limon148 <mehadihasan.cseru@gmail.com>
2020-03-16 02:03:11 +0600

    3. Creates tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -10,6 +10,17 @@ contract TodoList {
 
     mapping(uint => Task) public tasks;
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
     constructor() public {
         createTask("Hello there!");
     }
@@ -17,5 +28,12 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
+    // function toggleCompleted(uint _id) public {
+    //     Task memory _task = tasks[_id];
+    //     _task.completed = !_task.completed;
+    //     tasks[_id] = _task;
+    //     emit TaskCompleted(_id, _task.completed);
+    // }
 }
